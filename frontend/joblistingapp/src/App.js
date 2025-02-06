bc38dcc5e45a94a18f6f016a5a36cdf70b427aa1
Solanki-Raja <princesolanki770@gmail.com>
2025-02-05 21:58:53 -0800

    Code changes in App.js, Joblist.js

--- frontend/joblistingapp/src/App.js
+++ frontend/joblistingapp/src/App.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import JobList from "./JobList";
-// import JobDetail from "./JobDetail";
-
-
-// function App() {
-//   const pathname = window.location.pathname;
-//   const isJobDetail = pathname.startsWith("/job/");
-
-//   return (
-//     <div>
-//       {isJobDetail ? <JobDetail /> : <JobList />}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import JobList from "./JobList";
 import JobDetail from "./JobDetail";
